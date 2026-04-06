ea7425ab2dccfc44534b8b696cd98615c6ca9d18
Akanksha <a31695132@gmail.com>
2026-04-06 15:53:09 +0530

    REACT CW

--- react-js/src/propsPassing/Child.jsx
+++ react-js/src/propsPassing/Child.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const Child = ({name,alert}) => {
+const Child = ({name,alert,hi}) => {
   return (
     <>
     <div>Child</div>
     <h1>Rendered in child component:{name}</h1>
     <button onClick={alert}>Click</button>
+    <p>Button 2</p>
+    <button onClick={hi}>Click ME Too</button>
+    <h2>Byeeee!</h2>
     </>
   )
 }
--- react-js/src/propsPassing/Parent.jsx
+++ react-js/src/propsPassing/Parent.jsx
@@ -7,11 +7,15 @@ const Parent=()=>{
     function Alert(){
         alert("BUTTON CLICKED");
     }
+    function Alert2(){
+        alert("button 2 clicked")
+    }
     return (
         <>
         <div>Parent</div>
         <h1>Rendered in parent component:{fullname}</h1>
-        <Child name={fullname} alert={Alert}/>
+        <Child name={fullname} alert={Alert} hi={Alert2}/>
+        
         </>
     )
 
